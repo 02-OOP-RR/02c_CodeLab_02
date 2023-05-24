68e3d0a7e1dc725a1579b1f21bac0ed27e9fc0fb
Roxana <roxanarevilla@outlook.de>
2023-05-24 16:54:35 +0200

    Übung_2_24_05_2023

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,18 @@
 package com.cc.java;
 
+
 public class App {
 
 	public static void main(String[] args) {
 	
+	Mitarbeiter mitarbeiter = new Mitarbeiter("Mütze", "Markus", "CEO", 2001);
+
+		output(mitarbeiter.getName());
+		output(mitarbeiter.getVorname());
+		output(mitarbeiter.getFunktion());
+
+		
+			
 	
 
 	}
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,57 @@
+package com.cc.java;
+
+
+public class Mitarbeiter {
+
+private String name;
+private String vorname;
+private String funktion;
+private int eintrittsjahr;
+
+
+public Mitarbeiter(String name, String vorname, String funktion, int eintrittsjahr) {
+    this.name = name;
+    this.vorname = vorname;
+    this.funktion = funktion;
+    this.eintrittsjahr = eintrittsjahr;
+}
+
+
+public String getInfo(String op) {
+    return null;
+
+    switch (op) {
+        case value:
+        case value:
+        case value:
+
+        default:
+    }
+    
+}
+
+public String getName() {
+    return name;
+}
+
+public String getVorname() {
+    return vorname;
+}
+
+
+public String getFunktion() {
+    return funktion;
+}
+
+
+public int getEintrittsjahr() {
+    return eintrittsjahr;
+}
+
+
+
+
+}
+
+    
+        
